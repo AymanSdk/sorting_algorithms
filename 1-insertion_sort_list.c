79c919542f082573e5723c136842a18c811803da
AymanSdk <ayman.sdk123@outlook.com>
2023-10-29 17:42:58 +0100

    fixing betty style

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,9 +2,9 @@
 
 /**
  * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
+ * @node1: A pointer to the first node to swap.
+ * @node2: The second node to swap.
+ * @head : head.
  * Return: void
  */
 void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2)
@@ -12,8 +12,8 @@ void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2)
 	(*node1)->next = node2->next;
 	if (node2->next != NULL)
 		node2->next->prev = *node1;
-	node2->prev = (*node1)->prev;
-	node2->next = *node1;
+		node2->prev = (*node1)->prev;
+		node2->next = *node1;
 	if ((*node1)->prev != NULL)
 		(*node1)->prev->next = node2;
 	else
@@ -33,17 +33,16 @@ void insertion_sort_list(listint_t **list)
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-        current = (*list)->next;
-        while (current != NULL)
-        {
-                temp = current->next;
-                insertion = current->prev;
-                while (insertion != NULL && current->n < insertion->n)
-                {
-                        swap_nodes(list, &insertion, current);
-                        print_list((const listint_t *)*list);
-                }
-                current = temp;
-        }
-	
+		current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current->next;
+		insertion = current->prev;
+	while (insertion != NULL && current->n < insertion->n)
+	{
+		swap_nodes(list, &insertion, current);
+		print_list((const listint_t *)*list);
+	}
+	current = temp;
+}
 }
